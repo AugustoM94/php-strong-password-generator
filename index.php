6025bb694a9c4745ef39371887c1ed6dfddc0d5e
AugustoM94 <email@github.com>
2023-11-30 15:29:46 +0100

    Milestone1 completed

--- index.php
+++ index.php
@@ -1,27 +1,38 @@
 <?php
-
 function generatePassword() {
-    
-    $simbols = '!?&%$><^+-\*/(){}@#_-=';
+    // array disponibili
+    $symbols = '!?&%$><^+-*/(){}@#_-=';
     $letters = 'abcdefghijklmnopqrstuvwxyz';
     $upLetters = strtoupper($letters);
     $numbers = '0123456789';
 
-   if(isset($_GET['passwordLength'])) {
-    $passwordLength = $_GET['passwordLength'];
-    $newPassword = '';
+    // Inizializzazione della password vuota
+    $password = '';
 
-    while(strlen($newPassword) < $passwordLength) {
-           $valoriDisponibili = $simbols. $letters. $upLetters. $numbers;
-           $newChars = $valoriDisponibili[rand(0, strlen($valoriDisponibili) -1)];
-           if (!str_contains($newPassword, $newChars)) {
-                $newPassword .= $newChars;
-      }
+    // Verifica se la lunghezza della password è stata fornita attraverso GET
+    if (isset($_GET['passwordLength'])) {
+        $passwordLength = $_GET['passwordLength'];
+
+        // Ciclo per generare la password fino alla lunghezza inserita in input
+        while (strlen($password) < $passwordLength) {
+            // Combinazione di tutti i caratteri disponibili
+            $availableChars = $symbols . $letters . $upLetters . $numbers;
+
+            // Estrazione casuale di un carattere
+            $newChar = $availableChars[rand(0, strlen($availableChars) - 1)];
+
+            // Aggiungere il nuovo carattere solo se non è già presente nella password
+            if (strpos($password, $newChar) === false) {
+                $password .= $newChar;
+            }
+        }
     }
-  }
-}
 
+    // Restituzione della password generata
+    return $password;
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -29,20 +40,28 @@ function generatePassword() {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/style.css">
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <title>PHP Strong Password Generator</title>
 </head>
 <body>
-    <main class="container text-center">
-            <h1>Strong Password Generate</h1>
-                <form action="index.php" method="GET">
-                        <label for="passwordLength"> Lunghezza della password:</label>
-                        <input type="number" name="passwordLength" id="passwordLength" min="8" max="25" required>
-                        <button type="submit">Genera Password</button>
-                </form>
-                <p>La tua password generata è: $newChars</p>
+<div class="container d-flex align-items-center justify-content-center vh-100">
+    <main class="text-center">
+        <h1 class="mb-4">Strong Password Generator</h1>
+            <h3>Genera una password sicura</h3>
+        <form action="index.php" method="GET">
+            <label for="passwordLength">Lunghezza della password:</label>
+            <input type="number" name="passwordLength" id="passwordLength" min="8" max="25" required>
+            <button type="submit" class="btn btn-primary">Genera Password</button>
+        </form>
+        <?php
+        // Controlla se è stata generata una password
+        if (isset($_GET['passwordLength'])) {
+            $generatedPassword = generatePassword();
+            echo "<p class='mt-3'>La tua password generata è: $generatedPassword</p>";
+        }
+        ?>
     </main>
-
+</div>
 </body>
 </html>
