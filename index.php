945774e08c4d56cf566959908720bfb48c1bdf39
AugustoM94 <email@github.com>
2023-11-30 15:42:32 +0100

    Milestone 2 completed

--- index.php
+++ index.php
@@ -1,36 +1,8 @@
 <?php
-function generatePassword() {
-    // array disponibili
-    $symbols = '!?&%$><^+-*/(){}@#_-=';
-    $letters = 'abcdefghijklmnopqrstuvwxyz';
-    $upLetters = strtoupper($letters);
-    $numbers = '0123456789';
 
-    // Inizializzazione della password vuota
-    $password = '';
+include __DIR__ . '/partials/functions.php';
 
-    // Verifica se la lunghezza della password è stata fornita attraverso GET
-    if (isset($_GET['passwordLength'])) {
-        $passwordLength = $_GET['passwordLength'];
-
-        // Ciclo per generare la password fino alla lunghezza inserita in input
-        while (strlen($password) < $passwordLength) {
-            // Combinazione di tutti i caratteri disponibili
-            $availableChars = $symbols . $letters . $upLetters . $numbers;
-
-            // Estrazione casuale di un carattere
-            $newChar = $availableChars[rand(0, strlen($availableChars) - 1)];
-
-            // Aggiungere il nuovo carattere solo se non è già presente nella password
-            if (strpos($password, $newChar) === false) {
-                $password .= $newChar;
-            }
-        }
-    }
-
-    // Restituzione della password generata
-    return $password;
-}
+$password = generatePassword();
 ?>
 
 
